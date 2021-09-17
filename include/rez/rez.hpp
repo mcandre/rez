3d11ea930b3b592eb03d7a243e0c6d5b671ec1bd
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-09-17 11:07:35 -0500

    bump to v0.0.5

--- include/rez/rez.hpp
+++ include/rez/rez.hpp
@@ -18,7 +18,7 @@ namespace rez {
 /**
  * @brief Version is semver.
  */
-constexpr auto Version = "0.0.4";
+constexpr auto Version = "0.0.5";
 
 /**
  * @brief TaskDefinitionCpp denotes the path to a C++ task definition source file.
