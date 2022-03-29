dbcba2415d10d7adc77955006357345303496af8
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2022-03-29 13:35:45 -0500

    bump to v0.0.10

--- include/rez/rez.hpp
+++ include/rez/rez.hpp
@@ -18,7 +18,7 @@ namespace rez {
 /**
  * @brief Version is semver.
  */
-constexpr char Version[]{ "0.0.9" };
+constexpr char Version[]{ "0.0.10" };
 
 /**
  * @brief TaskDefinitionCpp denotes the path to a C++ task definition source file.
