00d080fa7c2774346f9ffa1b87237ab9f22b0c1b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2023-05-02 22:03:54 -0500

    update unmake

--- install.mk
+++ install.mk
@@ -1,7 +1,9 @@
+.POSIX:
+.SILENT:
 .PHONY: all
 
 all:
-	@cargo install --force unmake@0.0.7
+	cargo install --force unmake@0.0.10
 
-	@pip3 install --upgrade pip setuptools
-	@pip3 install -r requirements-dev.txt
+	pip3 install --upgrade pip setuptools
+	pip3 install -r requirements-dev.txt
